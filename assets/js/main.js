18491e02d411a584422e924eaa50d7ceffa9b0f0
Hichem Chabou <41421720+Hichem-Chabou@users.noreply.github.com>
2020-01-07 17:36:39 +0100

    lazyload images

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// navBar toggle
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+});
+
+// lazyLoad images
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+	const src = img.getAttribute("data-src");
+	if(!src) {
+		return;
+	}
+	img.src = src;
+}
+
+const imgOptions = {
+	threshold : 0,
+	rootMargin : "0px 0px 100px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+	entries.forEach(entry => {
+		if(!entry.isIntersecting) {
+			return;
+		}
+		else {
+			preloadImage(entry.target);
+			imgObserver.unobserve(entry.target);
+		}
+	})
+}, imgOptions);
+
+images.forEach(image => {
+	imgObserver.observe(image);
+});
